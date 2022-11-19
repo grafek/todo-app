deb33552b38a226cc76fde2f65f691d1d2b1aa49
grafek <jacek.grafender@gmail.com>
2022-11-19 14:40:18 +0100

    cleanup, react-icons install

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,5 @@
 import { router } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
-
-export const appRouter = router({
-  example: exampleRouter,
-  auth: authRouter,
-});
+export const appRouter = router({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
